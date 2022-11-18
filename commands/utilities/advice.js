860d307e935478c7b10174fea2644ec5edd8cdd5
ErrorDoc404 <akashsinhakvs@gmail.com>
2022-11-18 10:42:29 +0530

    utilities

---
+++ commands/utilities/advice.js
@@ -0,0 +1,30 @@
+const random = require("something-random-on-discord").Random;
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+  name: "advice",
+  description: "Get a random advice",
+  usage: "",
+  permissions: {
+    channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
+    member: [],
+  },
+  aliases: [],
+  category: "utilities",
+  SlashCommand: {
+    /**
+   *
+   * @param {import("../library/DiscordModerationBot")} client
+   * @param {import("discord.js").Message} message
+   * @param {string[]} args
+   * @param {*} param3
+   */
+    run: async (client, interaction) => {
+      let data = await random.getAdvice();
+      data = data.embed;
+      const embed = new MessageEmbed(data);
+
+      return interaction.reply({ embeds: [embed] }).catch((err) => client.error(err));
+    }
+  }
+};
---
+++ commands/utilities/meme.js
@@ -0,0 +1,30 @@
+const random = require("something-random-on-discord").Random;
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+  name: "meme",
+  description: "Get the good Meme",
+  usage: "",
+  permissions: {
+    channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
+    member: [],
+  },
+  aliases: ["memes"],
+  category: "utilities",
+  SlashCommand: {
+    /**
+ *
+ * @param {import("../library/DiscordModerationBot")} client
+ * @param {import("discord.js").Message} message
+ * @param {string[]} args
+ * @param {*} param3
+ */
+    run: async (client, interaction) => {
+      let data = await random.getMeme();
+      data = data.embed;
+      let meme = new MessageEmbed(data);
+
+      return interaction.reply({ embeds: [meme] }).catch((err) => client.error(err));
+    },
+  },
+};
